768c1852de9c8dc3e7641f1b6b8457c701d66a8d
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2022-11-15 16:21:38 -0300

    feat: responsive grid start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.scss'
 
 
 function Header() {
+  const [ activeLink, setActiveLink] = useState('home')
+
+  const [ scrolled, setScrolled] = useState(false)
+
+  useEffect(()=>{
+    const onScroll = () =>{
+      if(window.scrollY > 50){
+        setScrolled(true)
+      }else{
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", onScroll)
+
+    return() => window.removeEventListener("scroll", onScroll)
+  }, [])
+
+  const onUpdateActiveLink = (value) =>{
+    setActiveLink(value)
+  }
+
   return (
-    <header>
+    <nav className={scrolled ? 'scrolled':""}>
       <div className="logo">
         <p>Carlos Rodrigues</p>
       </div>
       <ul>
-        <li><a href="#">About Me</a></li>
-        <li><a href="#">My Jobs</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="/" className={activeLink === 'aboutme' ? 'active navvbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('aboutme')}>About Me</a></li>
+        <li><a href="/myjobs" className={activeLink === 'myjobs' ? 'active navvbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('myjobs')}>My Jobs</a></li>
+        <li><a href="/contact" className={activeLink === 'contact' ? 'active navvbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink('contact')}>Contact</a></li>
       </ul>
-    </header>
+      
+    </nav>
+    
   )
 }
 
