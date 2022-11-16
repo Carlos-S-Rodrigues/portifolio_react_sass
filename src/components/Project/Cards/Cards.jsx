accfbd8700e3c6883db69b6fcf6d2cae0b88d280
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2022-11-16 15:00:38 -0300

    feat: project grid done

--- src/components/Project/Cards/Cards.jsx
+++ src/components/Project/Cards/Cards.jsx
@@ -3,16 +3,17 @@ import "./styled.scss"
 
 function Cards(props) {
   return (
-    <section>
-      <div className="grid-wrapper">
-          <div className="design-card">
-            <img src={props.img} />
-            <h1 className="job-name">{props.name}</h1>
-            <p className="job-description"> {props.description}</p>
-            <button className="card-btn">View Project</button>
-          </div> 
+      
+      <div className="card">
+        <div className="card-body">
+          <img className="card-image" src={props.img} />
+          <h1 className="card-title">{props.title}</h1>
+          <p className="card-description">{props.description}</p>
+        </div>
+        <button className="card-btn">View Project</button>
       </div>
-    </section>    
+    
+     
   )
 }
 
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,15 +2,51 @@ import React from 'react'
 import Cards from './Cards/Cards'
 import './style.scss'
 
+
+
 function Project() {
 
   return (
     <div>
       <section className="projects">
         <h2>Here you can see some of my work, as a designer and developer.</h2>
-        <Cards
-          img="https://firebasestorage.googleapis.com/v0/b/portifolio-27d3f.appspot.com/o/uma-historia-totalmente-aleatoria-interativa-6353447-300820162245.jpg?alt=media&token=6af832d0-2bf6-44b8-9090-5b644b34cf33" 
-          name="Uma Nova História." description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium a alias aspernatur dolorem sint. Assumenda veniam eius illo explicabo ut reiciendis." />
+
+        <div className="card__wrapper">
+          <Cards
+            img="https://firebasestorage.googleapis.com/v0/b/portifolio-27d3f.appspot.com/o/uma-historia-totalmente-aleatoria-interativa-6353447-300820162245.jpg?alt=media&token=6af832d0-2bf6-44b8-9090-5b644b34cf33" 
+            title="The Project"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium a alias aspernatur dolorem sint. Assumenda veniam eius illo explicabo ut reiciendis." />
+
+            <Cards
+            img="https://firebasestorage.googleapis.com/v0/b/portifolio-27d3f.appspot.com/o/uma-historia-totalmente-aleatoria-interativa-6353447-300820162245.jpg?alt=media&token=6af832d0-2bf6-44b8-9090-5b644b34cf33" 
+            title="The Project"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium a alias aspernatur dolorem sint. Assumenda veniam eius illo explicabo ut reiciendis." />
+
+
+            <Cards
+            img="https://firebasestorage.googleapis.com/v0/b/portifolio-27d3f.appspot.com/o/uma-historia-totalmente-aleatoria-interativa-6353447-300820162245.jpg?alt=media&token=6af832d0-2bf6-44b8-9090-5b644b34cf33" 
+            title="The Project"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium a alias aspernatur dolorem sint. Assumenda veniam eius illo explicabo ut reiciendis." />
+
+
+            <Cards
+            img="https://firebasestorage.googleapis.com/v0/b/portifolio-27d3f.appspot.com/o/uma-historia-totalmente-aleatoria-interativa-6353447-300820162245.jpg?alt=media&token=6af832d0-2bf6-44b8-9090-5b644b34cf33" 
+            title="The Project"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium a alias aspernatur dolorem sint. Assumenda veniam eius illo explicabo ut reiciendis." />
+
+
+            <Cards
+            img="https://firebasestorage.googleapis.com/v0/b/portifolio-27d3f.appspot.com/o/uma-historia-totalmente-aleatoria-interativa-6353447-300820162245.jpg?alt=media&token=6af832d0-2bf6-44b8-9090-5b644b34cf33" 
+            title="The Project"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium a alias aspernatur dolorem sint. Assumenda veniam eius illo explicabo ut reiciendis." />
+
+
+            <Cards
+            img="https://firebasestorage.googleapis.com/v0/b/portifolio-27d3f.appspot.com/o/uma-historia-totalmente-aleatoria-interativa-6353447-300820162245.jpg?alt=media&token=6af832d0-2bf6-44b8-9090-5b644b34cf33" 
+            title="The Project"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium a alias aspernatur dolorem sint. Assumenda veniam eius illo explicabo ut reiciendis." />
+
+          </div>
       </section> 
     </div>
   )
